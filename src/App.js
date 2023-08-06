ef5161c3a9571e9a514b0449083b20a3741863db
David Damasceno <davidmdlp@gmail.com>
2023-08-06 20:01:27 -0300

    Inicio do Projeto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { FiSearch } from 'react-icons/fi';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <a href='/'><img src="https://somapay.com.br/wp-content/themes/Somapay/assets/images/logo-colorida-rebranding.svg" alt=""></img></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon d-flex justify-content-center align-items-center"><i class="fas fa-bars fs-24"></i></span>
+            </button>
+    </div>
+  );
+}
+
+export default App;
