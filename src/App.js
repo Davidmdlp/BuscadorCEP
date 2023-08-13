8eace255d4e612e48b26e662fe0caa093082dbad
David Damasceno <davidmdlp@gmail.com>
2023-08-13 11:26:53 -0300

    Removendo referências da Somapay

--- src/App.js
+++ src/App.js
@@ -35,12 +35,6 @@ function App() {
 
   return (
     <div className="container">
-      {/* <div className="title">
-      <a href='/'><img src="https://somapay.com.br/wp-content/themes/Somapay/assets/images/logo-colorida-rebranding.svg" alt=""></img></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon d-flex justify-content-center align-items-center"><i class="fas fa-bars fs-24"></i></span>
-            </button>
-      </div> */}
       <div className='title'>
         BUSCADOR CEP
       </div>
